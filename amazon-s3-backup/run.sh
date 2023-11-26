3a8bb7643e721c1cc8d8cb0690d2955c3f362676
dataatsky <you@example.com>
2023-11-26 13:54:22 +0100

    fixing cp

--- amazon-s3-backup/run.sh
+++ amazon-s3-backup/run.sh
@@ -17,6 +17,6 @@ export AWS_REGION="$bucket_region"
 
 bashio::log.debug "Using AWS CLI version: '$(aws --version)'"
 bashio::log.debug "Command: 'aws s3 cp $monitor_path s3://$bucket_name/ --no-progress --region $bucket_region --storage-class $storage_class'"
-aws s3 cp $monitor_path s3://"$bucket_name"/ --no-progress --region "$bucket_region" --storage-class "$storage_class"
+aws s3 cp $monitor_path s3://"$bucket_name"/ --no-progress --region "$bucket_region" --storage-class "$storage_class" --recursive
 
 bashio::log.info "Finished Amazon S3 Backup."
